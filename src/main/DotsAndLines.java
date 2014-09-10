1b1e3b581a89456ba9e6e0709a4e7644a734feff
Joseph Van Boxtel <javb1234@gmail.com>
2014-09-09 20:19:40 -0700

    added scoreboard and started checking command-line arguments.

--- src/main/DotsAndLines.java
+++ src/main/DotsAndLines.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
+import javax.swing.colorchooser.ColorSelectionModel;
 
 // change turns and scoring...........................................
 
@@ -19,10 +20,11 @@ public class DotsAndLines extends JFrame implements MouseListener{
 	public static final Color[] colors = new Color[] {Color.white, Color.red, Color.blue, Color.green, Color.orange, Color.pink, Color.cyan};
 	
 	// instance variables
-	int player = 1;
+	int player = 0;
 	int movesLeft = 1;
 	public int[] score;
 	public JLabel[] scoreLabels;
+	public JLabel turnLabel;
 	
 	public int[][] boardx = new int[boardSize][boardSize +2];
 	public int[][] boardy = new int[boardSize +2][boardSize];
@@ -30,30 +32,38 @@ public class DotsAndLines extends JFrame implements MouseListener{
 	
 	public DotsAndLines() {
 		super("Dots And Lines Game");
-		setSize( (boardSize * space) + thickness, (boardSize * space) + thickness );
+		setSize( (boardSize * space) + thickness * 2, (boardSize * space) + thickness + space );
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		DrawPanel displayPanel = new DrawPanel(this);
+		JPanel scorePanel = new JPanel();
+		turnLabel = new JLabel();
+		scorePanel.add(turnLabel);
 		score = new int[players];
 		scoreLabels = new JLabel[players];
 		for (int i = 0; i < scoreLabels.length; i++) {
 			
 			scoreLabels[i] = new JLabel(Integer.toString(score[i]));
 			scoreLabels[i].setForeground(colors[i+1]);
-			displayPanel.add(scoreLabels[i]);
+			scoreLabels[i].setVisible(true);
+			scorePanel.add(scoreLabels[i]);
 		}
-		
-		add(displayPanel);
+		JPanel panel = new JPanel();
+		BorderLayout box = new BorderLayout();
+		panel.setLayout(box);
+		panel.add(scorePanel, box.NORTH);
+		panel.add(displayPanel, box.CENTER);
+		add(panel);
 		displayPanel.addMouseListener(this);
 		setVisible(true);
 		setupGame();
 	}
 	
 	private void setupGame() {
-		
+		switchPlayer();
 	}
 	
-	public void isSquare(int type, int x, int y, int player) {
+	public void checkSquare(int type, int x, int y, int player) {
 		if (type == X_AXIS) {
 			// if checking from top
 			if (y < boardSize &&  boardx[x][y] > 0 && boardx[x][y +1] > 0 && boardy[x][y] > 0 && boardy[x +1][y] > 0) {
@@ -85,11 +95,13 @@ public class DotsAndLines extends JFrame implements MouseListener{
 	}
 	
 	private void switchPlayer() {
-		if (player == players) {
+		if (player >= players) {
 			player = 1;
 		} else {
 			player += 1;
 		}
+		turnLabel.setText("player " + player + "'s turn.");
+		turnLabel.setForeground(colors[player]);
 	}
 
 	@Override
@@ -107,29 +119,25 @@ public class DotsAndLines extends JFrame implements MouseListener{
 			if (boardy[(int) x / space] [(int) y / space] == 0) {
 				// clicked on the West.
 				boardy[(int) x / space] [(int) y / space] = player;
-				isSquare(Y_AXIS, (int) x / space, (int) y / space, player);
-				movesLeft -= 1;
-				if (movesLeft < 1) {
-					switchPlayer();
-					movesLeft = 1;
-				}
-			}	
-				
-			
-			
-		} else if ( (int) y % space <= thickness) {
-			// checks if spot is taken
-			if (boardx[(int) x / space] [(int) y / space] == 0) {
-				// clicked on the North
-				boardx[(int) x / space] [(int) y / space] = player;
-				isSquare(X_AXIS, (int) x / space, (int) y / space, player);
+				checkSquare(Y_AXIS, (int) x / space, (int) y / space, player);
 				movesLeft -= 1;
 				if (movesLeft < 1) {
 					switchPlayer();
 					movesLeft = 1;
 				}
 			}
-			
+		} else if ( (int) y % space <= thickness) {
+			// checks if spot is taken
+			if (boardx[(int) x / space] [(int) y / space] == 0) {
+				// clicked on the North
+				boardx[(int) x / space] [(int) y / space] = player;
+				checkSquare(X_AXIS, (int) x / space, (int) y / space, player);
+				movesLeft -= 1;
+				if (movesLeft < 1) {
+					switchPlayer();
+					movesLeft = 1;
+				}
+			}
 		}
 		int totalScore = 0;
 		int winningPlayer = 0;
@@ -160,10 +168,15 @@ public class DotsAndLines extends JFrame implements MouseListener{
 			boardSize = Integer.parseInt(args[0]);
 			thickness = Integer.parseInt(args[1]);
 			players = Integer.parseInt(args[2]);
+			
 		} else {
 			boardSize = 10;
 			thickness = 16;
 			players = 2;
+		}
+		if (players >= colors.length || boardSize > 15){
+			JOptionPane.showMessageDialog(null, "comand-line argument(s) invalid", "Error", JOptionPane.ERROR_MESSAGE);
+			System.exit(ERROR);
 		}
 		space = thickness * 4;
 		
@@ -185,7 +198,7 @@ class DrawPanel extends JPanel {
 		thickness = parent.thickness;
 		lineLength = space - thickness;
 	}
-	
+	@Override
 	public void paintComponent(Graphics comp) {
 		super.paintComponent(comp);
 		Graphics2D comp2D = (Graphics2D) comp;
@@ -221,7 +234,6 @@ class DrawPanel extends JPanel {
 			for (int y = 0; y < parent.board[x].length; y++) {
 				comp2D.setColor( parent.colors[parent.board[x][y] ] );
 				comp2D.fillRect(x * space + thickness + (thickness / 2), y * space + thickness + (thickness / 2), thickness * 2, thickness * 2);
-				
 			}
 		}
 		comp2D.setColor(Color.BLACK);
