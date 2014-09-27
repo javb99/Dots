aef3b63e4ac5aead9a36a6207b5ff06ba19f2b63
James Van Boxtel <jamesvanboxtel@gmail.com>
2014-09-27 08:35:31 -0700

    Moved simple computer player into its own class. Time to make advanced computers and battle!

--- src/client/ComputerPlayer.java
+++ src/client/ComputerPlayer.java
@@ -10,20 +10,17 @@ public class ComputerPlayer implements Player {
 	
 	@Override
 	public void turn(int player) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void move(int player, int axis, int x, int y) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void square(int player, int x, int y) {
-		// TODO Auto-generated method stub
-		
+	
 	}
 
 }
---
+++ src/client/SimpleComputer.java
@@ -0,0 +1,40 @@
+package client;
+
+import utilities.Constants;
+
+public class SimpleComputer extends ComputerPlayer {
+
+	SimpleComputer(BoardController controller) {
+		super(controller);
+	}
+
+	@Override
+	public void turn(int player) {
+		
+		if(player == boardController.getPlayerNumber()) {
+			int boardSize = boardController.getBoardSize();
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			findMove:
+			for(int axis=Constants.X_AXIS; axis<=Constants.Y_AXIS; ++axis) {
+				int xSize = axis == Constants.Y_AXIS ? boardSize+1 : boardSize;
+				for(int x=0; x<xSize; ++x) {
+					int ySize = axis == Constants.X_AXIS ? boardSize+1 : boardSize;
+					for(int y=0; y<ySize; ++y) {
+						int owner = boardController.getOwnerLine(axis, x, y);
+						if(owner == 0) {
+							boardController.playLine(axis, x, y);
+							break findMove;
+						}
+					}
+				}
+			}
+		}
+	}
+	
+}
